9429c088b2e408fe4be2c17e9c351bbdc55750b4
Nicholas Ng <onenylxus@gmail.com>
2024-02-11 16:21:07 +0800

    Add SVG to landing screen

--- screens/LandingScreen.js
+++ screens/LandingScreen.js
@@ -1,5 +1,6 @@
 import { Pressable, SafeAreaView, Text, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import TransferSvg from '../assets/transfer.svg';
 
 const LandingScreen = () => {
   const navigation = useNavigation();
@@ -10,12 +11,13 @@ const LandingScreen = () => {
 
   return (
     <SafeAreaView className="flex flex-1 px-8 py-16 items-center justify-end bg-zinc-50">
+      <TransferSvg width="80%" />
       <Text className="font-inter font-bold text-4xl text-center leading-[38px] text-teal-600">
         Spend Smarter{'\n'}Save More
       </Text>
       <Pressable className="w-full h-14 mt-6" onPress={onPress}>
         <View className="flex h-14 rounded-full items-center justify-center bg-teal-600">
-          <Text className="font-inter font-semibold text-teal-50">
+          <Text className="font-inter font-semibold text-lg text-teal-50">
             Get Started
           </Text>
         </View>
